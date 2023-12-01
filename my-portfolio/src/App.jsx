c5d361e638450408401e29b466a9bcbabd7af049
adam-korczak <adam.korczak@rutgers.edu>
2023-12-01 16:26:59 -0500

    first commit

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Intro from "./components/Intro";
+import Portfolio from "./components/Portfolio";
+import Timeline from "./components/Timeline";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+
+
+function App() {
+
+  return (
+      <div className = "App">
+        <Intro />
+        <Portfolio />
+        <Timeline />
+        <Contact />
+        <Footer />
+
+      </div>
+  )
+}
+
+export default App
---
+++ my-portfolio/src/components/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Intro() {
+    return (
+        <div className = "flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <h1 className = "text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Adam Korczak</h1>
+            <p className = "text-base md:text-xl mb-3 font-medium">Software Engineer</p>
+            <p className = "text-sm max-w-xl mb-6 font-bold">Hi. I'm a developer based in the NYC metro area, graduating from Rutgers University in January 2024 with a Bachelors in Information Technology & Informatics with a minor in Computer Science with software development as the overall track and focus. </p>
+        </div>
+    )
+}
+
+export default Intro;
