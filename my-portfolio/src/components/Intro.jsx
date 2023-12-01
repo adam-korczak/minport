15b9e3547242fc9b52695230ab5faa9650267004
adam-korczak <adam.korczak@rutgers.edu>
2023-12-01 18:07:59 -0500

    Adjusted padding, borders, iterations for portfolio items.

--- my-portfolio/src/components/Intro.jsx
+++ my-portfolio/src/components/Intro.jsx
@@ -5,7 +5,8 @@ function Intro() {
         <div className = "flex items-center justify-center flex-col text-center pt-20 pb-6">
             <h1 className = "text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Adam Korczak</h1>
             <p className = "text-base md:text-xl mb-3 font-medium">Software Engineer</p>
-            <p className = "text-sm max-w-xl mb-6 font-bold">Hi. I'm a developer based in the NYC metro area, graduating from Rutgers University in January 2024 with a Bachelors in Information Technology & Informatics with a minor in Computer Science with software development as the overall track and focus. </p>
+            <p className = "text-sm max-w-xl mb-3 font-bold">Hi. I'm a developer based in the NYC metro area, graduating from Rutgers University in January 2024 with a Bachelors in Information Technology & Informatics with a minor in Computer Science with software development as the overall track and focus.</p>
+            <p className = "text-sm max-w-xl mb-6 font-bold">Some of my professional experiences include previously working as a software engineer intern with GNC on internal tools and systems and working as a Teaching Assistant for the Computer Science department at Rutgers University.</p>
         </div>
     )
 }
---
+++ my-portfolio/src/data/Portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Minimalist Portfolio',
+        imgUrl: 'https://picsum.photos/200/300',
+        stack: ['JavaScript', 'React', 'Vite', 'Tailwind'],
+        link: 'https://github.com/adam-korczak/minport',
+    } ,
+    {
+        title: 'Movie Review App',
+        imgUrl: 'https://picsum.photos/200/300',
+        stack: ['Java', 'Spring Boot', 'React', 'MongoDB'],
+        link: 'https://github.com/adam-korczak/Movie-App',
+    } ,
+    {
+        title: 'Robotics Test Environment',
+        imgUrl: 'https://picsum.photos/200/300',
+        stack: ['Python', 'SciPy', 'Matplotlib'],
+        link: 'https://github.com/adam-korczak/Robotics-Collision-Test-Environment',
+    } ,
+]
