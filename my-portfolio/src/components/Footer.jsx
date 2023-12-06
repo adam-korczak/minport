1070f61ad07274216547c5cfad077396ec76638d
adam-korczak <adam.korczak@rutgers.edu>
2023-12-06 13:50:55 -0500

    UI Fixes, Links added
    Fixed conflicts with styling between light & dark modes, added links to footer.

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -4,8 +4,31 @@ function Footer() {
   return (
     <div className="py-5 text-center">
       {/* Social Icons */}
-      <p className="text-sm mt-2 opacity-50">
-        &copy; {new Date().getFullYear()} Adam Korczak. All rights reserved.
+      <p className="text-md mt-2 opacity-80">
+        <a
+          href="https://www.linkedin.com/in/adam-korczak"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="dark:text-white text-stone-900 hover:text-blue-700 dark:hover:text-blue-700"
+        >
+          LinkedIn |
+        </a>
+        <a
+          href="https://github.com/adam-korczak"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="dark:text-white text-stone-900 hover:text-blue-700 dark:hover:text-blue-700"
+        >
+          | GitHub |
+        </a>
+        <a
+          href="https://emerald-hildegarde-30.tiiny.site/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="dark:text-white text-stone-900 hover:text-blue-700 dark:hover:text-blue-700"
+        >
+          | Resume
+        </a>
       </p>
     </div>
   );
--- my-portfolio/src/data/Portfolio.js
+++ my-portfolio/src/data/Portfolio.js
@@ -1,19 +1,19 @@
 export default [
   {
     title: "Dev Portfolio",
-    imgUrl: "https://placehold.co/250x300/png",
+    imgUrl: "https://placehold.co/350x300/png",
     stack: ["JavaScript", "React", "Vite", "Tailwind"],
     link: "https://github.com/adam-korczak/minport",
   },
   {
     title: "Movie Review App",
-    imgUrl: "https://placehold.co/250x300/png",
+    imgUrl: "https://placehold.co/350x300/png",
     stack: ["Java", "Spring Boot", "React", "MongoDB"],
     link: "https://github.com/adam-korczak/Movie-App",
   },
   {
     title: "Robotics Test Environment",
-    imgUrl: "https://placehold.co/250x300/png",
+    imgUrl: "https://placehold.co/350x300/png",
     stack: ["Python", "SciPy", "Matplotlib"],
     link: "https://github.com/adam-korczak/Robotics-Collision-Test-Environment",
   },
